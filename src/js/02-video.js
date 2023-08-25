d79e2fcc55ea76a9edfb0be0b3ccf494b76d0c4e
Vitaliy2773 <vitalii64773@gmail.com>
2023-08-24 17:20:22 -1100

    homework

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+const video = require("@vimeo/player");
 
+
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+
+const iFrame = document.querySelector("iframe")
+const LOCAL_KEY = "videoPlayer-current-time";
+
+const player = new Player(iFrame)  
+
+
+const onPlay = (data) => {
+localStorage.setItem(LOCAL_KEY, JSON.stringify(data.seconds)); 
+};
+
+player.on('timeupdate',throttle(onPlay, 1000)); 
+player.setCurrentTime(localStorage.getItem(LOCAL_KEY));
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,31 @@
+const throttle = require("lodash.throttle")
 
+import throttle from "lodash.throttle"
+const form = document.querySelector(".feedback-form")
+
+const btn = document.querySelector("button")
+const FEEDBACK_FORM = "feedback-form-state"
+
+
+
+const dataForm = {}
+
+const saveMessage = (e) => {
+    e.preventDefault();
+    dataForm[e.target.name] = e.target.value
+    console.log(dataForm);
+    localStorage.setItem(FEEDBACK_FORM, JSON.stringify(dataForm))
+}
+
+
+    const btnSubmit = (e) => {
+        e.preventDefault();
+    // e.currentTarget.reset()
+    localStorage.removeItem(FEEDBACK_FORM)
+    localStorage.getItem(FEEDBACK_FORM, JSON.stringify(dataForm))
+    console.log(dataForm);
+}
+
+form.addEventListener("input", throttle(saveMessage,500))
+
+btn.addEventListener("click", btnSubmit)
